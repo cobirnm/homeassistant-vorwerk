a9485616fc228ba95099741a8c07cd09ffa08d7a
cobirnm <45829329+cobirnm@users.noreply.github.com>
2025-06-12 09:52:10 +0100

    Update __init__.py
    Compatible with 2025.06

--- __init__.py
+++ __init__.py
@@ -104,7 +104,7 @@ async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool
 
     for component in VORWERK_PLATFORMS:
         hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
+            await hass.config_entries.async_forward_entry_setups(entry, [component])
         )
 
     return True
